e67189a08bd84e1763bac789d73b02bf2d09a6bb
James Newman <jamesmnewman1@gmail.com>
2014-09-29 15:31:08 -0700

    tdd & bdd

---
+++ vendor/bundle/gems/nokogiri-1.6.3.1/ext/nokogiri/siteconf20140929-73803-4doiyq.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20140929-73803-1d97d1s"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
