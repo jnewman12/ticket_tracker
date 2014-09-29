ac02d5c0d1345828915332a26caa23dbe675c55c
James Newman <jamesmnewman1@gmail.com>
2014-09-29 15:15:59 -0700

    set up

---
+++ vendor/bundle/gems/json-1.8.1/ext/json/ext/generator/siteconf20140929-72854-1cy6p6d.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20140929-72854-1kpbmr3"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
