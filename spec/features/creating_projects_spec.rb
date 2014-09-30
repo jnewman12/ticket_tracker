eb305df000b45d4aa13280f8ec2cf87dad49da46
James Newman <jamesmnewman1@gmail.com>
2014-09-29 20:37:22 -0700

    duh

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -8,5 +8,10 @@ feature 'Creating Projects' do
     fill_in 'Description', with: 'A text-editor for Mac and PC'
     click_button 'Create Project'
     expect(page).to have_content('Project has been created')
+
+     project = Project.where(name: "Sublime 2").first
+     expect(page.current_url).to eql(project_url(project))
+     title = "Sublime 2 - Projects - Ticket Tracker"
+     expect(page).to have_title(title)
   end
 end
