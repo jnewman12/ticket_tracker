a563ec13e4a0d1fa37ee297cf4a85db1b650e302
James Newman <jamesmnewman1@gmail.com>
2014-09-29 17:32:10 -0700

    tests

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -1,15 +1,12 @@
 require 'spec_helper'
 
 feature 'Creating Projects' do
-	scenario "can create a project" do
-		visit '/'
-
-		click_link "New Project"
-
-		fill_in "Name", with: "Sublime 2"
-		fill_in "Description", with: "A text editor for Mac and PC"
-		click_button "Create Project"
-
-		expect(page).to have_content('Project Has Been Created')
-	end
+  scenario "can create a project" do
+    visit '/'
+    click_link 'New Project'
+    fill_in 'Name', with: 'TextMate 2'
+    fill_in 'Description', with: 'A text-editor for OS X'
+    click_button 'Create Project'
+    expect(page).to have_content('Project has been created.')
+  end
 end
